5875c3040ba60e36c1f8fec8b9f4a183c195348f
carlos <carlosmast2301@hotmail.es>
2020-11-10 11:33:39 -0500

    Agregado cambios de propiedad a Codesoft

--- Readme.txt
+++ Readme.txt
@@ -1,6 +1,6 @@
 Thanks for downloading this template!
 
 Template Name: USAGO
-Template URL: https://bootstrapmade.com/USAGO-free-medical-bootstrap-theme/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
+Template URL: https://codesoft.com/USAGO-free-medical-bootstrap-theme/
+Author: codesoft.com
+License: https://codesoft.com/license/
--- forms/contact.php
+++ forms/contact.php
@@ -3,7 +3,7 @@
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
+  * For more info and help: https://codesoft.com/php-email-form/
   */
 
   // Replace info@usago.com with your real receiving email address
